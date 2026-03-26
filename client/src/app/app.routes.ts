5037a20c487102c3b2358fa9177ee2379ee8d4fc
maruf-pfc <mdmarufsarker.mms@gmail.com>
2026-03-27 02:44:20 +0600

    feat(client): implement login and registration UI

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { User } from './user/user';
+import { Login } from './user/login/login';
+import { Register } from './user/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: User,
+    children: [
+      { path: 'login', component: Login },
+      { path: 'register', component: Register },
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+    ],
+  },
+];
---
+++ client/src/app/user/user.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-user',
+  imports: [RouterOutlet],
+  templateUrl: './user.html',
+  styles: ``,
+})
+export class User {
+
+}
